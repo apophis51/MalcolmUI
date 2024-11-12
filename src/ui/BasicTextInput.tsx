4d813b4f26da9dd0a8c71ab59fbf2574d20a95c2
apophis51 <malcolmxvernon@hotmail.com>
2024-11-12 02:44:30 -0500

    advancedTextInput added

--- src/ui/BasicTextInput.tsx
+++ src/ui/BasicTextInput.tsx
@@ -5,14 +5,18 @@ import { useRef } from 'react'
 
 //let textOutput = ['']
 
+/**
+ * Represents a reference string that holds the current value.
+ */
 type referenceString = {
+    /** The current value of the reference string. */
     current: string
 }
 export default function useBasicTextInput({prompt}: {prompt: string}): [ referenceString, React.FC ] {
     // const [selectedOption, setSelectedOption] = useState(options[0]);
     // const [output, setOutput] = useState('');
     const textOutput = useRef<string>('') //we made it a useRef because defining this outside the function was causing racing conditions
-
+    
 
     
     function BasicTextInput() {
--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,3 +1,4 @@
 export { default as useBasicToggle } from './BasicToggle'
 export { default as useBasicSelect } from './BasicSelect'
 export { default as useBasicTextInput } from './BasicTextInput'
+export {default as useAdvancedTextInput} from './AdvancedTextInput'
